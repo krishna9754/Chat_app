4b48b5397485b5cf6ef650b0d877016d632d08dc
Krishna <102092559+krishna9754@users.noreply.github.com>
2024-06-01 13:54:31 -0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import "../src/style.css"
+import ChatContainer from './components/ChatContainer';
+
+function App() {
+  return (
+    <div className="App">
+    <ChatContainer />
+    </div>
+  )
+}
+
+export default App;
